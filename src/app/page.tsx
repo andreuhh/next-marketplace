1c8243ecd93f0fe40e5330f434319f67a22e4a94
Andrea Guffi <andreaguffi92@gmail.com>
2025-08-13 23:35:44 +0200

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 export default function Home() {
   return (
-    <div>
-      <Button variant={"destructive"}>
+    <div className="flex flex-col">
+      <Button variant={"elevated"}>
         hello
       </Button>
-      <h1 className="bg-amber-400">test</h1>
+      <Progress value={50} />
+      <Textarea placeholder="Im a text area" />
+      <Input />
     </div>
   )
 }

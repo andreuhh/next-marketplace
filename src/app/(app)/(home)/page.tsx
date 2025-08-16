262652cc762487b7d1a6698ef2b8691c75af56bf
Andrea Guffi <andreaguffi92@gmail.com>
2025-08-16 17:34:27 +0200

    mongo db and payload configuration

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,21 +1,19 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise
+  });
+
+  const data = await payload.find({
+    collection: 'categories',
+  })
 
   return (
-    <div className="flex flex-col">
-      <Button variant={"elevated"}>
-        hello
-      </Button>
-      <Progress value={50} />
-      <Textarea placeholder="Im a text area" />
-      <Checkbox />
-      <Input />
+    <div>
+      {JSON.stringify(data, null, 2)}
     </div>
   )
 }

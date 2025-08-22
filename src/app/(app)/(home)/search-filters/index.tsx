58f9ad41c1f6f45a402c36a0199896196fc9439b
Andrea Guffi <andreaguffi92@gmail.com>
2025-08-22 08:57:20 +0200

    small fix

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -13,7 +13,7 @@ export const SearchFilters = ({
     return (
         <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
             <SearchInput data={data} />
-            <div className="hidden lg-block">
+            <div className="hidden lg:block">
                 <Categories data={data} />
             </div>
         </div>

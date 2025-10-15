d6d2c0aef912e930462d99a37bdd976e0478f6a4
Andrea Guffi <andreaguffi92@gmail.com>
2025-10-15 22:54:17 +0200

    category

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,13 +1,41 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Where } from "payload";
+import { z } from "zod";
 
 export const productsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async ({ ctx }) => {
+    getMany: baseProcedure.input(
+        z.object({
+            category: z.string().nullable().optional(),
+        }),
+    )
+        .query(async ({ ctx, input }) => {
+            const where: Where = {};
 
-        const data = await ctx.db.find({
-            collection: 'products',
-            depth: 1, // Populate category & image
+            if (input.category) {
+                // const categoriesData = await ctx.db.find({
+                //     collection: "categories",
+                //     limit: 1,
+                //     pagination: false,
+                //     where: {
+                //         slug: {
+                //             equals: input.category
+                //         }
+                //     }
+                // });
+                // const category = categoriesData.docs[0];
 
-        });
-        return data;
-    }),
+                //if (category) {
+                where["category.slug"] = {
+                    equals: input.category
+                }
+                //}
+            }
+
+            const data = await ctx.db.find({
+                collection: 'products',
+                depth: 1, // Populate category & image
+                where
+            });
+            return data;
+        }),
 });
